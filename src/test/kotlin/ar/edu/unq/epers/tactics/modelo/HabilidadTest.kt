22ca9c09d1a10922f0053f9c0d932a47e05c86d4
Mauro Bosetti <amuro.bosetti@gmail.com>
2020-09-28 19:47:05 -0300

    Refactors varios luego de code review

--- src/test/kotlin/ar/edu/unq/epers/tactics/modelo/HabilidadTest.kt
+++ src/test/kotlin/ar/edu/unq/epers/tactics/modelo/HabilidadTest.kt
@@ -10,14 +10,13 @@ internal class HabilidadTest{
         val party = Party("Party")
         val aventureroEmisor = Aventurero(party, "Pepe", 10, 0, 0, 0, 10, 10)
         val aventureroReceptor = Aventurero(party, "Jorge", 25, 0, 0, 10, 0, 10)
-
-        assertThat(aventureroReceptor.vida()).isEqualTo(50)
+        val vidaAntesDelAtaque = aventureroReceptor.vida()
 
         val dadoDe20Falso = DadoDe20(10)
         val ataque = Ataque(aventureroEmisor.danio_fisico(), aventureroEmisor.precision_fisica(), aventureroReceptor, dadoDe20Falso)
         ataque.resolverParaReceptor(aventureroReceptor)
 
-        assertThat(aventureroReceptor.vida()).isLessThan(50)
+        assertThat(aventureroReceptor.vida()).isLessThan(vidaAntesDelAtaque)
     }
 
     @Test
@@ -25,14 +24,13 @@ internal class HabilidadTest{
         val party = Party("Party")
         val aventureroEmisor = Aventurero(party, "Pepe", 10, 0, 0, 0, 10, 10)
         val aventureroReceptor = Aventurero(party, "Jorge", 25, 0, 0, 70, 0, 10)
-
-        assertThat(aventureroReceptor.vida()).isEqualTo(170)
+        val vidaAntesDelAtaque = aventureroReceptor.vida()
 
         val dadoDe20Falso = DadoDe20(1)
         val ataque = Ataque(aventureroEmisor.danio_fisico(), aventureroEmisor.precision_fisica(), aventureroReceptor, dadoDe20Falso)
         ataque.resolverParaReceptor(aventureroReceptor)
 
-        assertThat(aventureroReceptor.vida()).isEqualTo(170)
+        assertThat(aventureroReceptor.vida()).isEqualTo(vidaAntesDelAtaque)
     }
 
     @Test
@@ -42,10 +40,11 @@ internal class HabilidadTest{
         val aventureroReceptor = Aventurero(party, "Jorge", 21, 0, 0, 20, 0, 10)
         val dadoDe20Falso = DadoDe20(20)
         val ataque = Ataque(aventureroEmisor.danio_fisico(), aventureroEmisor.precision_fisica(), aventureroReceptor, dadoDe20Falso)
+        val vidaAntesDelAtaque = aventureroReceptor.vida()
 
         ataque.resolverParaReceptor(aventureroReceptor)
 
-        assertThat(aventureroReceptor.vida()).isEqualTo(55)
+        assertThat(aventureroReceptor.vida()).isEqualTo(vidaAntesDelAtaque - aventureroEmisor.danio_fisico())
     }
 
     @Test
@@ -55,12 +54,13 @@ internal class HabilidadTest{
         val aventureroDefendido = Aventurero(party, "Jorge", 21, 0, 0, 20, 0, 10)
         val defensa = Defensa(aventureroDefensor, aventureroDefendido)
         val dadoDe20Falso = DadoDe20(20)
+        val vidaAntesDelAtaque = aventureroDefensor.vida()
 
         defensa.resolverParaReceptor(aventureroDefendido)
         val ataque = Ataque(aventureroDefendido.danio_fisico(), aventureroDefendido.precision_fisica(), aventureroDefensor, dadoDe20Falso)
         ataque.resolverParaReceptor(aventureroDefensor)
 
-        assertThat(aventureroDefensor.vida()).isEqualTo(4)
+        assertThat(aventureroDefensor.vida()).isEqualTo(vidaAntesDelAtaque - aventureroDefendido.danio_fisico() / 2)
     }
 
     @Test
@@ -70,12 +70,13 @@ internal class HabilidadTest{
         val aventureroDefendido = Aventurero(party, "Jorge", 21, 0, 0, 20, 0, 10)
         val defensa = Defensa(aventureroDefensor, aventureroDefendido)
         val dadoDe20Falso = DadoDe20(20)
+        val vidaAntesDelAtaque = aventureroDefendido.vida()
 
         defensa.resolverParaReceptor(aventureroDefendido)
         val ataque = Ataque(aventureroDefensor.danio_fisico(), aventureroDefensor.precision_fisica(), aventureroDefendido, dadoDe20Falso)
         ataque.resolverParaReceptor(aventureroDefendido)
 
-        assertThat(aventureroDefendido.vida()).isEqualTo(61)
+        assertThat(aventureroDefendido.vida()).isEqualTo(vidaAntesDelAtaque - aventureroDefensor.danio_fisico() / 2)
     }
 }
 
