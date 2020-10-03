e40fb53b82ccb4f1a4788def43effaa450b90b64
David Gonzalez <davidgonzalezjscosasunq@gmail.com>
2020-10-03 13:47:08 -0300

    Se borra metodo cambiarNombrePor
    Solo lo utiliza el aventurero (internamente) para cambiar el valor del "nombre"

--- src/main/kotlin/ar/edu/unq/epers/tactics/modelo/Aventurero.kt
+++ src/main/kotlin/ar/edu/unq/epers/tactics/modelo/Aventurero.kt
@@ -120,7 +120,7 @@ class Aventurero(
         this.destreza = aventureroDTO.atributos.destreza
         this.constitucion = aventureroDTO.atributos.constitucion
         this.fuerza = aventureroDTO.atributos.fuerza
-        this.cambiarNombrePor(aventureroDTO.nombre)
+        this.nombre = aventureroDTO.nombre
         this.recalcularVidaYMana()
     }
 
@@ -128,11 +128,7 @@ class Aventurero(
         vida = ((nivel() * 5) + (constitucion * 2) + fuerza)
         mana = nivel() + inteligencia
     }
-
-    internal fun cambiarNombrePor(nuevoNombre: String) {
-        this.nombre = nuevoNombre
-    }
-
+    
     fun registarseEn(party: Party) {
         this.party = party
     }
