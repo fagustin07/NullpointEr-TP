f2653d991ceadde3a524577900400f611f7b16a0
David Gonzalez <davidgonzalezjscosasunq@gmail.com>
2020-10-08 02:46:12 -0300

    Separacion del dado simulado y el dado real

--- src/test/kotlin/ar/edu/unq/epers/tactics/modelo/habilidades/AtaqueTest.kt
+++ src/test/kotlin/ar/edu/unq/epers/tactics/modelo/habilidades/AtaqueTest.kt
@@ -2,6 +2,7 @@ package ar.edu.unq.epers.tactics.modelo.habilidades
 
 import ar.edu.unq.epers.tactics.modelo.Aventurero
 import ar.edu.unq.epers.tactics.modelo.Party
+import ar.edu.unq.epers.tactics.modelo.dado.DadoSimulado
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -23,7 +24,7 @@ internal class AtaqueTest {
     fun `un ataque con tirada mayor a la armadura mas la mitad de la velocidad del receptor es exitoso`() {
         val vidaAntesDelAtaque = aventureroReceptor.vida()
 
-        val dadoDe20Falso = DadoDe20(10)
+        val dadoDe20Falso = DadoSimulado(10)
         val ataque = Ataque.para(aventureroEmisor, aventureroReceptor, dadoDe20Falso)
         ataque.resolverse()
 
@@ -34,7 +35,7 @@ internal class AtaqueTest {
     fun `un ataque con tirada menor a la armadura mas la mitad de la velocidad del receptor falla`() {
         val vidaAntesDelAtaque = aventureroReceptor.vida()
 
-        val dadoDe20Falso = DadoDe20(1)
+        val dadoDe20Falso = DadoSimulado(1)
         val ataque = Ataque.para(aventureroEmisor, aventureroReceptor, dadoDe20Falso)
         ataque.resolverse()
 
@@ -43,7 +44,7 @@ internal class AtaqueTest {
 
     @Test
     fun `un ataque exitoso le resta al receptor una cantidad de vida igual al daño fisico del emisor`() {
-        val dadoDe20Falso = DadoDe20(20)
+        val dadoDe20Falso = DadoSimulado(20)
         val ataque = Ataque.para(aventureroEmisor, aventureroReceptor, dadoDe20Falso)
         val vidaAntesDelAtaque = aventureroReceptor.vida()
 
