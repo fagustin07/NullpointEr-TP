63caa144a046e2b35a9ec539003f6e1cbc4cd40a
Mauro Bosetti <amuro.bosetti@gmail.com>
2020-10-26 21:07:37 -0300

    Llevo la logica de conectarse a Neo4J a otro objeto

--- src/main/kotlin/ar/edu/unq/epers/tactics/persistencia/dao/neo4j/Neo4JClaseDAO.kt
+++ src/main/kotlin/ar/edu/unq/epers/tactics/persistencia/dao/neo4j/Neo4JClaseDAO.kt
@@ -2,26 +2,13 @@ package ar.edu.unq.epers.tactics.persistencia.dao.neo4j
 
 import ar.edu.unq.epers.tactics.modelo.Clase
 import ar.edu.unq.epers.tactics.persistencia.dao.ClaseDAO
+import ar.edu.unq.epers.tactics.service.runner.Neo4JTransactionRunner
 import org.neo4j.driver.*
 
 class Neo4JClaseDAO: ClaseDAO {
 
-    private val driver: Driver
-
-    init {
-        val env = System.getenv()
-        val url = env.getOrDefault("NEO_URL", "bolt://localhost:7687")
-        val username = env.getOrDefault("NEO_USER", "neo4j")
-        val password = env.getOrDefault("NEO_PASSWORD", "root")
-
-        driver = GraphDatabase.driver(url, AuthTokens.basic(username, password),
-            Config.builder().withLogging(Logging.slf4j()).build()
-        )
-    }
-
     override fun crear(clase: Clase): Clase {
-        driver.session().use { session ->
-
+        Neo4JTransactionRunner().runTrx{ session ->
             session.writeTransaction {
                 val query = "MERGE (n:Clase {nombre: ${'$'}nombre})"
                 it.run(query, Values.parameters(
@@ -42,7 +29,7 @@ class Neo4JClaseDAO: ClaseDAO {
     }
 
     override fun recuperarTodas(): List<Clase> {
-        return driver.session().use { session ->
+        return Neo4JTransactionRunner().runTrx{ session ->
             val result = session.run("MATCH (n:Clase) RETURN n")
             result.list{ record ->
                 Clase(record[0]["nombre"].asString())
@@ -51,7 +38,7 @@ class Neo4JClaseDAO: ClaseDAO {
     }
 
     fun clear() {
-        return driver.session().use { session ->
+        Neo4JTransactionRunner().runTrx{ session ->
             session.run("MATCH (n) DETACH DELETE n")
         }
     }
