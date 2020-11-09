1c0a431b8b95a914f7413edce4a0279ed8ae2948
fagustin07 <federico.sandoval@alu.unq.edu.ar>
2020-11-09 10:39:36 -0300

    planteo test de crearFormacion

--- src/main/kotlin/ar/edu/unq/epers/tactics/modelo/Formacion.kt
+++ src/main/kotlin/ar/edu/unq/epers/tactics/modelo/Formacion.kt
@@ -1,6 +1,8 @@
 package ar.edu.unq.epers.tactics.modelo
 
 import org.bson.codecs.pojo.annotations.BsonProperty
+
+import org.bson.codecs.pojo.annotations.BsonProperty
 import org.bson.types.ObjectId
 
 class Formacion {
