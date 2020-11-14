c63d70ebc5b56e1ee0b16441df9583110a2a5d63
David Gonzalez <davidgonzalezjscosasunq@gmail.com>
2020-11-14 03:06:02 -0300

    Quito annotations de Hibernate que habia metido por error en Formacion

--- src/main/kotlin/ar/edu/unq/epers/tactics/modelo/Formacion.kt
+++ src/main/kotlin/ar/edu/unq/epers/tactics/modelo/Formacion.kt
@@ -2,21 +2,13 @@ package ar.edu.unq.epers.tactics.modelo
 
 import org.bson.codecs.pojo.annotations.BsonProperty
 import org.bson.types.ObjectId
-import javax.persistence.ElementCollection
-import javax.persistence.MapKey
-import javax.persistence.OneToMany
 
 class Formacion {
 
     @BsonProperty("_id")
     var id : ObjectId? = null
     var nombre : String? = null
-
-    @ElementCollection
-    @OneToMany
-    @MapKey(name = "requerimiento")
     var requerimientos : Map<String, Int> = mutableMapOf()
-
     var stats : List<AtributoDeFormacion> = mutableListOf()
     var id: ObjectId? = null
     var nombre: String? = null
