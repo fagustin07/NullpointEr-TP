f0da12ebd973002f64b96fd73b5bcce9da02f8ca
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2020-12-07 00:52:53 -0300

    change signature para guardar fechas al registrar compras

--- src/main/kotlin/ar/edu/unq/epers/tactics/persistencia/dao/OperacionesDAO.kt
+++ src/main/kotlin/ar/edu/unq/epers/tactics/persistencia/dao/OperacionesDAO.kt
@@ -3,9 +3,10 @@ package ar.edu.unq.epers.tactics.persistencia.dao
 import ar.edu.unq.epers.tactics.modelo.tienda.Compra
 import ar.edu.unq.epers.tactics.modelo.tienda.Item
 import ar.edu.unq.epers.tactics.modelo.tienda.InventarioParty
+import java.time.LocalDate
 
 interface OperacionesDAO: DataDAO {
-    fun registrarCompraDe(inventarioParty: InventarioParty, item: Item)
+    fun registrarCompraDe(inventarioParty: InventarioParty, item: Item, fechaDeCompra: LocalDate)
 
     fun comprasRealizadasPorParty(nombreDeParty: String): List<Compra>
 }
