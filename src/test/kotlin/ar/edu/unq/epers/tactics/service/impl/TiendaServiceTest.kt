68f6684202acadb1f288f019819b1250ec4aa96a
David Gonzalez <davidgonzalezjscosasunq@gmail.com>
2020-12-11 10:59:24 -0300

    Se arregla tests fragil (fallaba si se ejecutaba muy rapido)

--- src/test/kotlin/ar/edu/unq/epers/tactics/service/impl/TiendaServiceTest.kt
+++ src/test/kotlin/ar/edu/unq/epers/tactics/service/impl/TiendaServiceTest.kt
@@ -325,7 +325,7 @@ class TiendaServiceTest {
 
         tiendaService.tradear(party.nombre(), partyCompradora.nombre(), listOf(item), 10)
 
-        almanaqueSimulado.cambiarFechaActual(LocalDateTime.now())
+        almanaqueSimulado.cambiarFechaActual(almanaqueSimulado.ahora().plusDays(1))
         tiendaService.registrarCompra(party.nombre(), item.nombre)
 
         assertThat(tiendaService.losItemsDe(party.nombre()))
